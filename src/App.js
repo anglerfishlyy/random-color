4430fa6b562ccf1b57eeec9587a54c1c1227b2d0
anglerfishlyy <kavyasamudralaA7@outlook.com>
2024-06-02 13:58:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Index from './index.jsx'; // Capitalized Index
+
+function App() {
+  return (
+    <div className="App">
+      <Index /> {/* Capitalized Index */}
+    </div>
+  );
+}
+
+export default App;
